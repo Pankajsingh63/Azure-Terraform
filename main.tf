a8b0e360ec869e0c1d5b46715a084f32c809a7e9
Pankajsingh63 <34594483+Pankajsingh63@users.noreply.github.com>
2023-05-02 20:31:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "myVM1" {
+resource "aws_instance" "myVM01" {
   ami           = "ami-00b2c40b15619f518"
   instance_type = "t2.micro"
   tags = {
@@ -11,18 +11,7 @@ resource "aws_instance" "myVM1" {
   }
 }
 
-resource "aws_instance" "myVM2" {
-  provider      = aws.west
-  ami           = "ami-0b695b365bec60938"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Example-02"
-    PatchGroup= "Group1"
-    Environment = "Dev"
-    CreatedOn = "01-May-2023"
-    CreatedBy = "Kunal K"
-  }
-}
+
 
 resource "aws_s3_bucket" "myBucket" {
   bucket = "terraform-practest"
